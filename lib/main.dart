99316cb5e9fcb5879b8f68900a93b4bcb80516db
lilykrasnogorova <krasnogorova0304@mail.ru>
2023-01-01 18:26:19 +0300

    Added simple class Transaction

--- lib/main.dart
+++ lib/main.dart
@@ -13,16 +13,42 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List a = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Flutter App'),
       ),
-      body: Column (children: [
-        Card(child: Text("Card")), 
-        Card(child: Text("List of TX")), 
-      ],),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Container(
+              width: double.infinity,
+              child: Card(
+                color: Colors.purple,
+                child: Text("Card",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                    )),
+                elevation: 5,
+              )),
+          Container(
+              width: 100,
+              child: Card(
+                color: Colors.purple,
+                child: Text("List of tx",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                    )),
+                elevation: 5,
+              )),
+        ],
+      ),
     );
   }
 }
